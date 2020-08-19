4383fd21c248ef32b09cddfdd6d94b234022eec8
Viacheslav <s.likhogub@gmail.com>
2020-08-19 15:50:49 +0300

    Added example with button

--- src/blinky.c
+++ src/blinky.c
@@ -7,22 +7,45 @@ struct pin led = {
     .port = AB_GPIOD,
     .num = 12,
     .dir = GPIO_MODE_OUTPUT,
-    .mode = GPIO_OTYPE_PP,
+    .mode = 0,
     .otype = GPIO_OTYPE_PP,
     .speed = GPIO_OSPEED_2MHZ,
     .pull_up_down = GPIO_PUPD_NONE,
     .is_inverse = false
 };
 
+
 struct pin led_pwm = {
     .port = AB_GPIOD,
     .num = 13,
     .dir = GPIO_MODE_OUTPUT,
-    .mode = GPIO_OTYPE_PP,
+    .mode = 0,
     .otype = GPIO_OTYPE_PP,
     .speed = GPIO_OSPEED_2MHZ,
     .pull_up_down = GPIO_PUPD_NONE,
     .is_inverse = false
+};
+
+struct pin button = {
+    .port = AB_GPIOA,
+    .num = 0,
+    .dir = GPIO_MODE_INPUT,
+    .mode = 0,
+    .otype = GPIO_OTYPE_PP,
+    .speed = GPIO_OSPEED_2MHZ,
+    .pull_up_down = GPIO_PUPD_NONE,
+    .is_inverse = false
+};
+
+struct pin led_but = {
+    .port = AB_GPIOD,
+    .num = 14,
+    .dir = GPIO_MODE_OUTPUT,
+    .mode = 0,
+    .otype = GPIO_OTYPE_PP,
+    .speed = GPIO_OSPEED_2MHZ,
+    .pull_up_down = GPIO_PUPD_NONE,
+    .is_inverse = true
 };
 
 int main(void)
@@ -30,12 +53,16 @@ int main(void)
     abst_init(16e6);
     abst_gpio_init(&led);
     abst_gpio_init(&led_pwm);
+    abst_gpio_init(&led_but);
+    abst_gpio_init(&button);
 
     abst_digital_write(&led, 1);
     int brig = 0;
     int cnt = 0;    
     while(1) {
         abst_delay_ms(1e3);
+        
+        // Soft PWM
         brig += 50;
         if (brig > 255) 
             brig = 0;
@@ -43,6 +70,11 @@ int main(void)
             abst_stop_pwm_soft(&led_pwm);
         else
             abst_pwm_soft(&led_pwm, brig);
+        
+        // Blinking
         abst_toggle(&led);
+
+        // A button that contors a led
+        abst_digital_write(&led_but, abst_digital_read(&button));
     }
 }
